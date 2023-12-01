22b71d530fbce942a57a914f9d3e4df551c84aa5
Enric Perpinyà Pitarch <56732262+evilmonkey19@users.noreply.github.com>
2023-12-01 19:33:34 +0100

    fixed preload

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -10,8 +10,8 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/fonts/IBMPlexSans-Bold.woff2" as="font" type="font/woff2" />
-        <link rel="preload" href="/fonts/IBMPlexSans-Regular.woff2" as="font" type="font/woff2" />
+        <link rel="preload" href="/fonts/IBMPlexSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+        <link rel="preload" href="/fonts/IBMPlexSans-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
         <title>Enric Perpinyà - Home</title>
       </head>
       <body className="bg-black">
