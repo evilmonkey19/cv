ecf8c4a570eb1bfa67c85620fa067ab774e1396f
Enric Perpinyà Pitarch <56732262+evilmonkey19@users.noreply.github.com>
2023-12-04 19:11:02 +0100

    Fix link rel attribute in _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, state }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Hi!👋 This is Enric and you about to look at my website. Enjoy!" />
         <meta name="author" content="Enric Perpinyà" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" defer/>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preload" href="/fonts/IBMPlexSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
