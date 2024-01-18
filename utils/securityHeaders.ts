c2138d4dae82a7971d8f4bb88204704be1c3d44d
Enric Perpinyà Pitarch <56732262+evilmonkey19@users.noreply.github.com>
2024-01-18 11:02:51 +0200

    Update security headers in utils/securityHeaders.ts

--- utils/securityHeaders.ts
+++ utils/securityHeaders.ts
@@ -1,8 +1,8 @@
 const contentSecurityPolicy = `
     default-src 'self'; 
     connect-src 'self'; 
-    script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; 
-    style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com;
+    script-src 'self' 'unsafe-inline'; 
+    style-src 'self' 'unsafe-inline';
 `;
 
 const SecurityHeaders = [
